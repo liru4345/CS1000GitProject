186323aea834e3cc24ebf7be733adf1d54424f1a
liru4345 <lizzie.ruff@colorado.edu>
2024-11-22 22:58:23 +0000

    I created a file reader in main that can print out all of our personal files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
-using namespace std;
+void readAndPrintFile(const std::string& filePath) {
+    std::ifstream file(filePath); // Open the file
+    if (file.is_open()) {
+        std::string line;
+        std::cout << "Contents of " << filePath << ":\n";
+        while (std::getline(file, line)) { // Read file line by line
+            std::cout << line << '\n';    // Print each line
+        }
+        file.close(); // Close the file
+        std::cout << "-------------------\n";
+    } else {
+        std::cerr << "Error: Could not open file " << filePath << '\n';
+    }
+}
 
-int main(int argc, char* argv[]){
-    std :: cout << "This is the test that the group code is working\n";
+int main() {
+    std::vector<std::string> fileNames; // Vector to store file names
+    int fileCount;
+
+    // Ask the user for the number of files
+    std::cout << "Enter the number of text files to read: ";
+    std::cin >> fileCount;
+
+    // Get the file names from the user
+    for (int i = 0; i < fileCount; ++i) {
+        std::string fileName;
+        std::cout << "Enter the name of file " << i + 1 << ": ";
+        std::cin >> fileName;
+        fileNames.push_back(fileName);
+    }
+
+    // Read and print the contents of each file
+    for (const auto& fileName : fileNames) {
+        readAndPrintFile(fileName);
+    }
+
     return 0;
 }
