8d34353fc209c0ce3933dfd51d328cd40160266a
liru4345 <lizzie.ruff@colorado.edu>
2024-11-22 22:44:59 +0000

    Save local changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
-main.cpp
-This is the main file
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    std :: cout << "This is the test that the group code is working\n";
+    return 0;
+}
